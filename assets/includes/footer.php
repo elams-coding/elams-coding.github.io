a6593e6010dc5743ed1c9593efe7a5a108e64ed8
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-08 22:30:11 +0100

    ajout des includes

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,9 +1,39 @@
 <footer>
     <div>
-        <a href="https://github.com/elams-coding">GitHub</a>
-        <a href="https://discord.com/users/1280435655817691189">Discord</a>
-        <a href="https://www.linkedin.com/in/el-amine-mahamoud-2914ba358">LinkedIn</a>
+        <h2>Liens vers mes réseaux</h2>
+        <ul>
+            <li>
+                <a href="https://github.com/elams-coding">
+                    <div>
+                        <img class="logo" src="assets/media/img/logo/github.png" alt="Logo de GitHub">
+                        <span>GitHub</span>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="https://discord.com/users/1280435655817691189">
+                    <div>
+                        <img class="logo" src="assets/media/img/logo/discord.png" alt="Logo de Discord">
+                        <span>Dicord</span>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/el-amine-mahamoud-2914ba358">
+                    <div>
+                        <img class="logo" src="assets/media/img/logo/linkedin.png" alt="Logo de LinkedIn">
+                        <span>LinkedIn</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
     </div>
 
-    <p>&copy; Elams-coding, 2025 - France</p>
+    <p>
+        &copy;
+        <?php
+            echo date("Y");
+        ?>
+        Elams-coding - France
+    </p>
 </footer>
--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,16 +1,4 @@
 <header>
+    <img id="mon-logo" class="logo" src="assets/media/img/logo/logo.png" alt="Logo de El Amine">
     <h1>Elams-coding's Portfolio</h1>
-    <nav>
-        <ul>
-            <li>
-                <a href="#projetsBox">Projets</a>
-            </li>
-            <li>
-                <a href="#competencesBox">Comp&eacute;tences</a>
-            </li>
-            <li>
-                <a href="#parcoursBox">Parcours</a>
-            </li>
-        </ul>
-    </nav>
 </header>
