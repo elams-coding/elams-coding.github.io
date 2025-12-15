0d358af9962f2f2eabdf3bce3f90ee491375954e
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-15 13:17:48 +0100

    Refaire le projet sans PHP

--- assets/includes/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <img id="mon-logo" class="logo" src="assets/media/img/logo/logo.png" alt="Logo de El Amine">
-    <h1>Elams-coding's Portfolio</h1>
-</header>
--- assets/includes/menu.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar container">
-    <a href="">A propos</a>
-    <a href="">Compétences</a>
-    <a href="">Parcours</a>
-    <a href="">Projets</a>
-</nav>
