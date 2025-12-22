0074fe0ada8e67f14df1dcca6f9713ebc026c161
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-22 16:25:57 +0100

    gestion de l'événement pour le header

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// animation of the (main) header
+const mainHeader = document.getElementById("main-header");
+
+let lastScroll = 0;
+let isTicking = false;
+
+window.addEventListener("scroll", () => {
+    if (!isTicking) {
+        window.requestAnimationFrame(() => {
+            const currentScroll = window.scrollY;
+            const threshold = 100;
+
+            if (currentScroll <= 0) {
+                mainHeader.classList.remove("scroll-down");
+            }
+            else if (currentScroll > lastScroll && currentScroll > threshold) {
+                mainHeader.classList.add("scroll-down");
+            }
+            else if (currentScroll < lastScroll) {
+                mainHeader.classList.remove("scroll-down");
+            }
+
+            lastScroll = currentScroll;
+            isTicking = false;
+        });
+
+        isTicking = true;
+    }
+});
