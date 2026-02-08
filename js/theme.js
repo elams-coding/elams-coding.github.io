811aaa20082a4a89b61d3ff503cdc7ab92c2902e
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2026-02-08 01:40:54 +0100

    Sauvegarde du thème
    Utilisation de localStorage pour la sauvegarde du thème.
    
    Améliore l'expérience utilisateur

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleTheme = document.getElementById('toggle-theme');
 
+    // créer 'theme' s'il n'existe pas
+    if (localStorage.getItem('theme') === null) {
+        localStorage.setItem('theme', 'light');
+    }
+
+    checkStatus();
+
+    function checkStatus() {
+        if (localStorage.getItem('theme') === 'dark') {
+            toggleTheme.classList.add('checked');
+        }
+        else {
+            toggleTheme.classList.remove('checked');
+        }
+
+        changeTheme();
+    }
+
+    function changeTheme() {
+        if (toggleTheme.classList.contains('checked')) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+            toggleTheme.setAttribute('aria-checked', 'dark');
+
+            // sauvegarder le thème
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+            toggleTheme.setAttribute('aria-checked', 'light');
+
+            // sauvegarder le thème
+            localStorage.setItem('theme', 'light');
+        }
+    }
+
+    // changer le thème
     toggleTheme.addEventListener('click', () => {
         toggleTheme.classList.toggle('checked');
 
-        if (toggleTheme.classList.contains('checked')) {
-            document.documentElement.setAttribute('data-bs-theme', 'dark');
-            toggleTheme.setAttribute('aria-checked', true);
-        } else {
-            document.documentElement.setAttribute('data-bs-theme', 'light');
-            toggleTheme.setAttribute('aria-checked', false);
-        }
+        changeTheme();
     });
 });
