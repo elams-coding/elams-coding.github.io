26d38f76d338ed1e94b1042a3faaa444b6680d77
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2026-05-07 00:39:57 +0200

    feat: ajout d'un composant VueJS

---
+++ .vitepress/theme/ExpandableDetail.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const open = ref(false)
+</script>
+
+<template>
+  <div class="expandable-detail">
+    <button class="expand-btn" :aria-expanded="open" @click="open = !open">
+      <span>{{ open ? 'Réduire' : 'En savoir plus' }}</span>
+      <span class="expand-icon" :class="{ 'expand-icon--open': open }">›</span>
+    </button>
+    <div v-show="open" class="expand-content">
+      <slot />
+    </div>
+  </div>
+</template>
