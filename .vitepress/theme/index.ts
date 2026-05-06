b751a4a7c55e834d5a0773fcbb8450b5ad181620
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2026-05-07 01:05:33 +0200

    fix: injection du PageAside pour le panneau latéral des pages formations et compétences techniques

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,16 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import ExpandableDetail from './ExpandableDetail.vue'
+import PageAside from './PageAside.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'aside-outline-after': () => h(PageAside)
+    })
+  },
   enhanceApp({ app }: { app: any }) {
     app.component('ExpandableDetail', ExpandableDetail)
   }
